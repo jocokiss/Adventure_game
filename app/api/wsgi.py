319ad25ef797d47a619e70ba92c8d7997a19c2ec
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2025-01-22 18:06:13 +0100

    fix(API): fix wsgi.py

--- app/api/wsgi.py
+++ app/api/wsgi.py
@@ -1,9 +1,6 @@
-import sys
-from pathlib import Path
-
-# Add the project root to PYTHONPATH
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-
 from app.api import create_app
 
-application = create_app()
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
