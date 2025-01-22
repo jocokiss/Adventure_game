c3d67839aec4bf40c74a1b9a7e1c981c29029080
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2025-01-22 18:41:52 +0100

    fix(API): fix wsgi.py

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,6 @@ from app.api.routes import api  # Adjust the import based on your structure
 
 
 def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(api)  # Register your routes
-    return app
+    application = Flask(__name__)
+    application.register_blueprint(api)  # Register your routes
+    return application
