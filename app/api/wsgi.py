ad773ce0ddf03c1564bacb347a5054479822fe9e
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2025-01-22 18:32:23 +0100

    fix(API): fix wsgi.py

--- app/api/wsgi.py
+++ app/api/wsgi.py
@@ -1,10 +1,7 @@
-try:
-    from . import create_app
-except ImportError:
-    raise ImportError("Unable to import create_app from __init__.py")
+from app.api import create_app
 
 
-app = create_app()
+application = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
