7fa733be34bfa4dfbab9d99615bb83183d0d9f5a
Jozsef Kiss <Jozsef.Kiss@redsailtechnologies.com>
2025-01-24 14:33:31 +0100

    feat(MONGO-DB): create MongoDB connection
    Create connection and a method to insert data into the database

--- src/utilities/database.py
+++ src/utilities/database.py
@@ -1,18 +1,38 @@
-import os
-from dotenv import load_dotenv
-
-import pymongo
-
+from pymongo import MongoClient
 from src.utilities.api_actions import GameAPIClient
 
 
+class MongoDBManager:
+    def __init__(self):
+        self.uri = GameAPIClient().get_mongo_token()
+        self.client = MongoClient(self.uri)
+        self.db = self.client['AdventureGame']
+
+    def insert_batch(self, collection_name, data: list[dict]):
+        """
+        Insert multiple documents into a MongoDB collection.
+
+        Args:
+            collection_name (str): The name of the collection.
+            data (list): List of dictionaries to insert.
+
+        Returns:
+            InsertManyResult: Result of the insert operation.
+        """
+        if data:
+            collection = self.db[collection_name]
+            result = collection.insert_many(data)
+            return result
+        self.close_connection()
+
+    def close_connection(self):
+        """Close the MongoDB connection."""
+        self.client.close()
+
 
 if __name__ == "__main__":
-    load_dotenv()
-    token_key = os.getenv('TOKEN_KEY')
-    print(token_key)
-
-
-    # api_client = GameAPIClient()
-    # mongo_string = api_client.mongo_string()
-    # print(mongo_string)
+    db = MongoDBManager()
+    db.insert_batch('MovementData', [{
+        'coordinates': '(0, 0)',
+        'time_spent': 6,
+    }])
